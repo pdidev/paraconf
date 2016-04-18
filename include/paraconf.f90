d8388627d731636881792bc7c401373ad675131a
Gaalich Mohamed <mohamed.gaalich@cea.fr>
2016-04-18 15:06:17 +0200

    first version of fortran interface

--- include/paraconf.f90
+++ include/paraconf.f90
@@ -21,7 +21,91 @@
 ! * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 ! * THE SOFTWARE.
 ! ******************************************************************************/
+ MODULE PC_tree_t
+
+  use iso_C_binding
+
+  TYPE, bind(C) :: PC_tree_t_f
+    INTEGER(C_INT) :: status
+    TYPE(C_PTR) :: document
+    TYPE(C_PTR) :: node
+  END TYPE PC_tree_t_f
+
+ END MODULE
 
  MODULE paraconf
+
+ USE iso_C_binding
+ USE PC_tree_t
+
+ IMPLICIT NONE
+
+  INTERFACE
+    FUNCTION PC_parse_path_f(path) &
+      bind(C, name="PC_parse_path")   
+      USE iso_C_binding 
+      USE PC_tree_t
+      TYPE(PC_tree_t_f) :: PC_parse_path_f
+      TYPE(C_PTR), VALUE :: path
+    END FUNCTION PC_parse_path_f
+  END INTERFACE
+
+  INTERFACE
+    FUNCTION PC_get_f(tree,index_fmt) &
+      bind(C, name="PC_get")   
+      USE iso_C_binding 
+      USE PC_tree_t
+      TYPE(PC_tree_t_f) :: PC_get_f
+      TYPE(PC_tree_t_f), VALUE :: tree
+      TYPE(C_PTR), VALUE :: index_fmt
+    END FUNCTION PC_get_f
+  END INTERFACE
+
+  INTERFACE
+    FUNCTION PC_len_f(tree,value) &
+      bind(C, name="PC_len")   
+      USE iso_C_binding 
+      USE PC_tree_t
+      INTEGER(C_INT) :: PC_len_f
+      TYPE(PC_tree_t_f), VALUE :: tree
+      INTEGER(C_INT) :: value
+    END FUNCTION PC_len_f
+  END INTERFACE
+
+  INTERFACE
+    FUNCTION PC_int_f(tree,value) &
+      bind(C, name="PC_int")   
+      USE iso_C_binding 
+      USE PC_tree_t
+      INTEGER(C_INT) :: PC_int_f
+      TYPE(PC_tree_t_f), VALUE :: tree
+      INTEGER(C_INT) :: value
+    END FUNCTION PC_int_f
+  END INTERFACE
+
+  INTERFACE
+    FUNCTION PC_double_f(tree,value) &
+      bind(C, name="PC_double")   
+      USE iso_C_binding 
+      USE PC_tree_t
+      INTEGER(C_INT) :: PC_double_f
+      TYPE(PC_tree_t_f), VALUE :: tree
+      REAL(C_DOUBLE) :: value
+    END FUNCTION PC_double_f
+  END INTERFACE
+
+  INTERFACE
+    FUNCTION PC_string_f(tree,value) &
+      bind(C, name="PC_string")   
+      USE iso_C_binding 
+      USE PC_tree_t
+      INTEGER(C_INT) :: PC_string_f
+      TYPE(PC_tree_t_f), VALUE :: tree
+      TYPE(C_PTR) :: value
+    END FUNCTION PC_string_f
+  END INTERFACE
+
+
+
 
  END MODULE paraconf
