25e740d6d52c1c7fbb9b637989dbf188663b33c7
Julien Bigot <julien.bigot@cea.fr>
2016-07-02 01:21:06 +0200

    Corrected examples

--- example/example.c
+++ example/example.c
@@ -11,16 +11,28 @@ int main(int argc, char *argv[])
 
 	PC_tree_t conf = PC_parse_path("example.yml");
 	
-	int nb_iter; PC_int(PC_get(conf, ".iter"), &nb_iter);
-
-	printf("nb_iter = %d\n",nb_iter);
-
-	char *nb_iter_string; PC_string(PC_get(conf, ".iter"), &nb_iter_string);
-
-	printf("nb_iter = %s\n",nb_iter_string);
-
-	free(nb_iter_string);
-	PC_tree_destroy(&conf);
-
+	int a_int; PC_int(PC_get(conf, ".a_int"), &a_int);
+	assert(a_int==100);
+	double a_float; PC_double(PC_get(conf, ".a_float"), &a_float);
+	assert(a_float==100.1);
+	char* a_string; PC_string(PC_get(conf, ".a_string"), &a_string);
+	assert(!strcmp("this is a string", a_string));
+	free(a_string);
+	int a_list_len; PC_len(PC_get(conf, ".a_list"), &a_list_len);
+	assert(a_list_len==2);
+	int a_list_0; PC_int(PC_get(conf, ".a_list[0]"), &a_list_0);
+	assert(a_list_0==10);
+	int a_map_len; PC_len(PC_get(conf, ".a_map"), &a_map_len);
+	assert(a_map_len==2);
+	char *a_map_0_k; PC_string(PC_get(conf, ".a_map{0}"), &a_map_0_k);
+	assert(!strcmp("first", a_map_0_k));
+	free(a_map_0_k);
+	int a_map_0_v; PC_int(PC_get(conf, ".a_map<0>"), &a_map_0_v);
+	assert(a_map_0_v == 20);
+	int another_list_1; PC_int(PC_get(conf, ".another_list[1]"), &another_list_1);
+	assert(another_list_1==31);
+	int another_map_second; PC_int(PC_get(conf, ".another_map.second"), &another_map_second);
+	assert(another_map_second==41);
+	
 	return 0;
 }
--- example/example.f90
+++ example/example.f90
@@ -1,33 +1,74 @@
 PROGRAM example
-	USE paraconf
+  USE paraconf
 
-	TYPE(PC_tree_t) :: tree1,tree2
-	INTEGER :: nb_iter, lengh
-	CHARACTER(20) :: nb_iter_string
-	REAL(8) :: nb_iter_double
+  TYPE(PC_tree_t) :: tree1,tree2
+  INTEGER :: a_int
+  CHARACTER(20) :: a_string
+  REAL(8) :: a_float
 
-	call PC_parse_path("example.yml",tree1)
+  call PC_parse_path("example.yml",tree1)
 
-	print *, "tree.status = ", tree1%status
+  call PC_get(tree1,".a_int", tree2)
+  call PC_int(tree2, a_int)
+  if ( a_int /= 100 ) then
+    print *, "error with a_int, ", a_int
+    stop
+  endif
+  call PC_get(tree1,".a_float", tree2)
+  call PC_double(tree2, a_float)
+  if ( a_float /= 100.1 ) then
+    print *, "error with a_float, ", a_float
+    stop
+  endif
+  call PC_get(tree1,".a_string", tree2)
+  call PC_string(tree2, a_string)
+  if ( a_string /= "this is a string" ) then
+    print *, "error with a_string, ", a_string
+    stop
+  endif
+  call PC_get(tree1, ".a_list", tree2)
+  call PC_len(tree2, a_int)
+  if ( a_int /= 2 ) then
+    print *, "error with a_list len, ", a_int
+    stop
+  endif
+  call PC_get(tree1, ".a_list[0]", tree2)
+  call PC_int(tree2, a_int)
+  if ( a_int /= 10 ) then
+    print *, "error with a_list[0], ", a_int
+    stop
+  endif
+  call PC_get(tree1, ".a_map", tree2)
+  call PC_len(tree2, a_int)
+  if ( a_int /= 2 ) then
+    print *, "error with a_map len, ", a_int
+    stop
+  endif
+  call PC_get(tree1,".a_map{0}", tree2)
+  call PC_string(tree2, a_string)
+  if ( a_string /= "first" ) then
+    print *, "error with a_map{0}, ", a_string
+    stop
+  endif
+  call PC_get(tree1, ".a_map<0>", tree2)
+  call PC_int(tree2, a_int)
+  if ( a_int /= 20 ) then
+    print *, "error with a_map<0>, ", a_int
+    stop
+  endif
+  call PC_get(tree1, ".another_list[1]", tree2)
+  call PC_int(tree2, a_int)
+  if ( a_int /= 31 ) then
+    print *, "error with another_list[1], ", a_int
+    stop
+  endif
+  call PC_get(tree1, ".another_map.second", tree2)
+  call PC_int(tree2, a_int)
+  if ( a_int /= 41 ) then
+    print *, "error with another_map.second, ", a_int
+    stop
+  endif
 
-	call PC_get(tree1,".iter",tree2)
-
-	call PC_int(tree2,nb_iter)
-
-	print *, "nb_iter =", nb_iter
-
-	call PC_len(tree2,lengh)
-
-	print *, "lengh(nb_iter) =", lengh
-
-	call PC_double(tree2,nb_iter_double)
-
-	print *, "nb_iter_double =", nb_iter_double
-
-	call PC_string(tree2,nb_iter_string)
-
-	print *, "nb_iter_string =", trim(nb_iter_string)
-
-	call PC_tree_destroy(tree1)
+  call PC_tree_destroy(tree1)
 
 END PROGRAM example
